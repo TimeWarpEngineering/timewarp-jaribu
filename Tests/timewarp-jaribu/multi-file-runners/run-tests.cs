e396480bfdfd853a45b9e29f205d52000ccb53c0
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2026-02-09 02:37:47 +0700

    Add multi-file runner entry points (run-tests.cs, ci-runner)

---
+++ Tests/timewarp-jaribu/multi-file-runners/run-tests.cs
@@ -0,0 +1,11 @@
+#!/usr/bin/dotnet --
+#:project ../../../Source/TimeWarp.Jaribu/TimeWarp.Jaribu.csproj
+
+// Multi-file test runner
+// Runs all test files compiled together via Directory.Build.props.
+// Test classes are auto-registered via [ModuleInitializer].
+
+WriteLine("Running all Jaribu tests");
+WriteLine();
+
+return await RunAllTests();
